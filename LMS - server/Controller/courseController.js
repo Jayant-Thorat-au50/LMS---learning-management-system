2d0b4a5e40133c92488f7adefd4219c308623b03
jayant thorat <jayantthorat1999@gmail.com>
2024-12-05 11:46:12 +0530

    add basic files for the courses

---
+++ LMS - server/Controller/courseController.js
@@ -0,0 +1,2 @@
+import CourseModel from "../Models/CouresModel";
+
--- LMS - server/Models/CouresModel.js
+++ LMS - server/Models/CouresModel.js
@@ -1,9 +1,16 @@
 import { Schema, model } from "mongoose";
 
 const couresSchema = new Schema({
-
+title:{
+    type:String,
+    require:[true, 'course title is reqiuired'],
+    trim:true,
+    minLength:[8, 'title should be at least 8 characters'],
+    maxLength:[50, 'title should not be  more than 50 characters'],
+}
 })
 
+// creating the course model with schema declared above and new collection 'course
 CourseModel = model('course', couresSchema, 'course');
 
 export default CourseModel;
