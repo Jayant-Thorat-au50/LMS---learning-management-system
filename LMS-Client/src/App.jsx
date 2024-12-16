bdf785bffcf13af2005efd1795c9b3fd295bdd08
jayant thorat <jayantthorat1999@gmail.com>
2024-12-16 20:13:52 +0530

    add basic dependancies

--- LMS-Client/src/App.jsx
+++ LMS-Client/src/App.jsx
@@ -1,14 +1,10 @@
-
-
 import React from 'react'
 import './App.css'
 
 function App() {
   return (
     <>
-  <h1 className=''>
-    lms
-  </h1>
+    <h1 className=' text-xl bg-orange-600'>lms fronted</h1>
     </>
   )
 }
