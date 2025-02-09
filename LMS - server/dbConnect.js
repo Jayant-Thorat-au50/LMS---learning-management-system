28b43c5a4bc1a0a78e08e210d5b8e05704e6614b
jayant thorat <jayantthorat1999@gmail.com>
2025-02-10 01:51:23 +0530

    edited connection string

--- LMS - server/dbConnect.js
+++ LMS - server/dbConnect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const mongodbUri = 'mongodb+srv://jayantthorat1999:taw3KkZcGgmuqBUN@cluster0.zvmkz.mongodb.net/LMS'
-
 const dbConnect =  () => {
       mongoose.connect(mongodbUri)
       .then(() => console.log('db connected successfully')
