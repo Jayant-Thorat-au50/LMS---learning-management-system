fdc780754cf6114bbabcb3cc02e59f9d417ca834
jayant thorat <jayantthorat1999@gmail.com>
2025-02-10 11:58:43 +0530

    frontend url update

--- LMS - server/app.js
+++ LMS - server/app.js
@@ -17,7 +17,7 @@ const paymentRoutes = require("./Routes/PaymentRoutes.js");
 
 const app = express();
 app.use(cors({
-  origin:"https://venerable-bonbon-6cee05.netlify.app/",
+  origin:"https://venerable-bonbon-6cee05.netlify.app",
   credentials:true
 }));
 const PORT = process.env.PORT;
